51f740a4d1c2350db701f21e991f2eb38b7aac2c
Justin Scott <hello@jrs.dev>
2022-08-21 15:25:45 -0700

    Update NumberGuessingGame.cs

--- Assets/Scripts/NumberGuessingGame.cs
+++ Assets/Scripts/NumberGuessingGame.cs
@@ -11,7 +11,7 @@ public class NumberGuessingGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartGame();
     }
 
     // Update is called once per frame
@@ -19,7 +19,39 @@ public class NumberGuessingGame : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.UpArrow))
         {
-            //code
+            minValue = guess;
+            NextGuess();
         }
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            maxValue = guess;
+            NextGuess();
+        }
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("I guessed your number, I'm a genius!");
+            StartGame();
+        }
+    }
+
+    void StartGame()
+    {
+        minValue = 1;
+        maxValue = 100;
+        guess = 50;
+
+        Debug.Log("Welcome to the Number Guessing Game.");
+        Debug.Log("Guess a number between " + minValue + " and " + maxValue + ", but don't tell me");
+        Debug.Log("Is your number " + guess + "?");
+        Debug.Log("Press the Up Arrow, if your number is higher.");
+        Debug.Log("Press the Down Arrow, if your number is lower.");
+        Debug.Log("Press the Enter key, if I guessed your number.");
+        maxValue = maxValue + 1;
+    }
+
+    void NextGuess()
+    {
+        guess = (maxValue + minValue) / 2;
+        Debug.Log("Is the number higher or lower than " + guess);
     }
 }
